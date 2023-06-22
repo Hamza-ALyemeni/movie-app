d2e5af6406b3b14860534623160d3cd04b099d6d
Aloud <Aloud@tad.local>
2023-06-22 15:15:17 +0300

    displaying popular movies

--- js/script.js
+++ js/script.js
@@ -2,6 +2,48 @@ const global = {
     currentPage : window.location.pathname,
 };
 
+async function displayPopularMovies() {
+    const { results } = await fetchAPIData('movie/popular');;
+    results.forEach((movie) =>{
+        const div = document.createElement('div');
+        div.classList.add('card');
+        div.innerHTML = `
+            <a href="movie-details.html?id=${movie.id}">
+                ${
+                    movie.poster_path 
+                    ?`<img
+                    src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+                    class="card-img-top"
+                    alt="${movie.title}"
+                    />`
+                    : 
+                    `<img
+                    src="images/no-image.jpg"
+                    class="card-img-top"
+                    alt="Movie Title"
+                    />`
+                }
+            </a>
+            <div class="card-body">
+                <h5 class="card-title">${movie.title}</h5>
+                <p class="card-text">
+                <small class="text-muted">Release: ${movie.release_date}</small>
+                </p>
+                </div>
+           
+        `;
+        document.querySelector('#popular-movies').appendChild(div);
+    });
+}
+
+async function fetchAPIData(endpoint) {
+    const API_KEY = '98dc1c8d10a93c6261ae282211b55ffd';
+    const API_URL = 'https://api.themoviedb.org/3/';
+    const response  = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`);
+    const data = await response.json();
+    return data;
+}
+
 function highlightActveLinks() {
     const links = document.querySelectorAll('.nav-link');
     links.forEach((link) => {
@@ -15,7 +57,7 @@ function init() {
     switch (global.currentPage) {
         case '/':
         case '/index.html':
-            console.log('Home');
+            displayPopularMovies();
             break;
         case '/shows.html':
             console.log('Shows');
