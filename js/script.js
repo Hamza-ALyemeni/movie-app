6a30b7c2410308ac3614a440c87bceb1c62e19a2
Aloud <Aloud@tad.local>
2023-06-22 16:00:43 +0300

    displaying shows and spinners and shit

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const global = {
     currentPage : window.location.pathname,
 };
 
+// display 20 most popular movies
 async function displayPopularMovies() {
     const { results } = await fetchAPIData('movie/popular');;
     results.forEach((movie) =>{
@@ -20,7 +21,7 @@ async function displayPopularMovies() {
                     `<img
                     src="images/no-image.jpg"
                     class="card-img-top"
-                    alt="Movie Title"
+                    alt="${movie.title}"
                     />`
                 }
             </a>
@@ -36,12 +37,60 @@ async function displayPopularMovies() {
     });
 }
 
+// display 20 most popular tv shows
+async function displayPopularTvShows() {
+    const { results } = await fetchAPIData('tv/popular');;
+    results.forEach((show) =>{
+        const div = document.createElement('div');
+        div.classList.add('card');
+        div.innerHTML = `
+            <a href="movie-details.html?id=${show.id}">
+                ${
+                    show.poster_path 
+                    ?`<img
+                    src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+                    class="card-img-top"
+                    alt="${show.name}"
+                    />`
+                    : 
+                    `<img
+                    src="images/no-image.jpg"
+                    class="card-img-top"
+                    alt="${show.name}"
+                    />`
+                }
+            </a>
+            <div class="card-body">
+                <h5 class="card-title">${show.name}</h5>
+                <p class="card-text">
+                <small class="text-muted">Air Date: ${show.first_air_date}</small>
+                </p>
+                </div>
+           
+        `;
+        document.querySelector('#popular-shows').appendChild(div);
+    });
+}
 async function fetchAPIData(endpoint) {
     const API_KEY = '98dc1c8d10a93c6261ae282211b55ffd';
     const API_URL = 'https://api.themoviedb.org/3/';
+
+    showspinner();
+
     const response  = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`);
     const data = await response.json();
+
+    hidespinner();
+
     return data;
+}
+
+function showspinner() {
+    document.querySelector('.spinner').classList.add('show')
+}
+
+function hidespinner() {
+    document.querySelector('.spinner').classList.remove('show')
 }
 
 function highlightActveLinks() {
@@ -60,7 +109,7 @@ function init() {
             displayPopularMovies();
             break;
         case '/shows.html':
-            console.log('Shows');
+            displayPopularTvShows();
             break;
         case '/movie-details.html':
             console.log('Movie Details');
