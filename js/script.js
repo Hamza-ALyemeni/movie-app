d40d8e2a5d448f9d0e310f3278c2ec90b5d1e4b8
Aloud <Aloud@tad.local>
2023-06-25 17:34:26 +0300

    search function and custom alert

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 const global = {
     currentPage : window.location.pathname,
+    search : {
+        term:'',
+        type:'',
+        page:1,
+        totalPages:1
+    }
 };
 
 // display 20 most popular movies
@@ -216,6 +222,21 @@ async function displayShowDetails() {
 
     }
 
+    async function search() {
+        const queryString = window.location.search;
+        const urlParams = new URLSearchParams(queryString);
+
+        global.search.type = urlParams.get('type');
+        global.search.term = urlParams.get('search-term');
+
+        if (global.search.term !== '' && global.search.term !== null) {
+            const results = await searchAPIdata();
+            console.log(results);
+        }else{
+            showAlert('please enter a search term');
+        }
+    }
+
     // Display Slider Movies
     async function displaySlider() {
         const { results } = await fetchAPIData('movie/now_playing');
@@ -239,6 +260,7 @@ async function displayShowDetails() {
             initSwiper();
         });
     }
+
     function initSwiper() {
         const swiper = new Swiper('.swiper',{
             slidesPerView : 1,
@@ -278,12 +300,34 @@ async function fetchAPIData(endpoint) {
     return data;
 }
 
+async function searchAPIdata() {
+    const API_KEY = '98dc1c8d10a93c6261ae282211b55ffd';
+    const API_URL = 'https://api.themoviedb.org/3/';
+
+    showspinner();
+
+    const response  = await fetch(`${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}`);
+    const data = await response.json();
+
+    hidespinner();
+
+    return data;
+}
+
 function showspinner() {
     document.querySelector('.spinner').classList.add('show')
 }
 
 function hidespinner() {
     document.querySelector('.spinner').classList.remove('show')
+}
+
+function showAlert(message , className) {
+    const alertEl = document.createElement('div');
+    alertEl.classList.add('alert',className);
+    alertEl.appendChild(document.createTextNode(message));
+    document.querySelector('#alert').appendChild(alertEl);
+    setTimeout(() => alertEl.remove(), 3000);
 }
 
 function addCommas(number) {
@@ -316,7 +360,7 @@ function init() {
             displayShowDetails();
             break;
         case '/search.html':
-            console.log('search');
+            search();
             break;
        
     }
