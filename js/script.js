e033467336428dc505510e076cd5801531c4735b
Aloud <Aloud@tad.local>
2023-06-26 12:05:41 +0300

    pagination functionalty added

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@ const global = {
         term:'',
         type:'',
         page:1,
-        totalPages:1
+        totalPages:1,
+        totalResults:0
     }
 };
 
@@ -230,7 +231,12 @@ async function displayShowDetails() {
         global.search.term = urlParams.get('search-term');
 
         if (global.search.term !== '' && global.search.term !== null) {
-            const { results , page , total_pages} = await searchAPIdata();
+            const { results , page , total_pages , total_results} = await searchAPIdata();
+
+            global.search.page = page;
+            global.search.totalPages = total_pages;
+            global.search.totalResults = total_results;
+
             if (results.length === 0) {
                 showAlert('no results found');
                 return;
@@ -247,6 +253,11 @@ async function displayShowDetails() {
     }
 
     function displaySearchResults(results){
+        // clear previous results
+        document.querySelector('#search-results').innerHTML = '';
+        document.querySelector('#search-results-heading').innerHTML = '';
+        document.querySelector('#pagination').innerHTML = '';
+
         results.forEach((result) =>{
             const div = document.createElement('div');
             div.classList.add('card');
@@ -275,7 +286,49 @@ async function displayShowDetails() {
                     </div>
                
             `;
+
+            document.querySelector('#search-results-heading').innerHTML = `
+                    <h2>${results.length} of ${global.search.totalResults}
+                     resluts for ${global.search.term}</h2>
+            `;
+
             document.querySelector('#search-results').appendChild(div);
+        });
+
+        displayPagination();
+    }
+
+    function displayPagination() {
+        const div = document.createElement('div');
+        div.classList.add('pagination');
+        div.innerHTML =`
+            <button class="btn btn-primary" id="prev">Prev</button>
+            <button class="btn btn-primary" id="next">Next</button>
+            <div class="page-counter">Page ${global.search.page} of ${global.search.totalPages}</div>
+        `;
+
+        document.querySelector('#pagination').appendChild(div);
+
+        if (global.search.page == 1) {
+            document.querySelector('#prev').disabled = true;
+        } 
+        
+        if(global.search.page == global.search.totalPages){
+            document.querySelector('#next').disabled = true;
+        }
+
+        // Next Page 
+        document.querySelector('#next').addEventListener('click',async () =>{
+            global.search.page++;
+            const {results , totalPages} = await searchAPIdata();
+            displaySearchResults(results);
+        });
+
+        // Prev Page
+        document.querySelector('#prev').addEventListener('click',async () =>{
+            global.search.page--;
+            const {results , totalPages} = await searchAPIdata();
+            displaySearchResults(results);
         });
     }
     // Display Slider Movies
@@ -347,7 +400,7 @@ async function searchAPIdata() {
 
     showspinner();
 
-    const response  = await fetch(`${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}`);
+    const response  = await fetch(`${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}&page=${global.search.page}`);
     const data = await response.json();
 
     hidespinner();
